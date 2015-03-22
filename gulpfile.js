66be44a366295150bf02b49660b6b7db6075a552
Kai Curry <kai@sundaysenergy.com>
2015-03-22 13:20:42 -0500

    SITE_ID global

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// This app info.
+var appInfo = require('./package.json')
+// Create global variable SITE_ID based on name property.
+global.SITE_ID = appInfo.name
 // Require coffeescript
 require('coffee-script/register');
 // Require cape-gulp that contains common tasks.
